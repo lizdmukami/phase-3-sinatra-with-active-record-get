387614408b20ea92b18c747d6ea4ee958e95600f
lizdmukami <lizdmukami@gmail.com>
2023-02-25 20:24:43 +0300

    commit tree 467d0d1298207632b39c032b6e75e9898d856d87

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,32 @@
 class ApplicationController < Sinatra::Base
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  set :default_content_type, 'application/json'
+  
+  # get '/' do
+  #   { message: "Hello world" }.to_json
+  # end
+
+  get '/games' do
+    # get all games from the database
+    games = Game.all.order(:title).limit(10)
+    # return a JSON response with an array of all the game data
+    games.to_json
+  end
+
+
+  #use the :id syntax to create a dynamic route
+  get '/games/:id' do
+    #look up the game in the databse using its ID
+    game = Game.find(params[:id])
+    #send a JSON-formatted respone of the game data
+    #include associated reviews in the JSON response
+    #game.to_json(include: :reviews)
+    #game.to_json(include: {reviews: { include: :user } })
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
   end
 
 end
